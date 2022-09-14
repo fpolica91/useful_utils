ad4bcca0682997fd88c03ac5ac6316deb1f4fda5
Fabricio <fabriciopolicarpo0@gmail.com>
2022-09-14 11:21:35 -0400

    Update simple.cpp

--- STL2/simple.cpp
+++ STL2/simple.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 vector<int> nums = {1,2,2,2,2,3,4,6};
 binary_search(nums.begin(), nums.end());
-
 // returns first occurence of elem
 // if elem not present points to next greater element
 // if not present returns last index. returns >= elem
